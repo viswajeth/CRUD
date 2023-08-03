22be466f943d41db063ae2d584211ac8f9b44442
viswajeth <92245051+viswajeth@users.noreply.github.com>
2023-08-03 23:23:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const {google} = require('googleapis')
+const keys=require('./credentials.json');
+const { ObjectId } = require('mongodb');
+
+
+
+const client = new google.auth.JWT(
+  keys.client_email,
+  null,
+  keys.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets']
+);
+
+client.authorize(function(err,tokens){  
+   
+  if(err){
+      console.log(err);
+      return;
+  }else{
+      console.log("Connected!");
+      gsrun(client)
+  }
+});
+const app = express();
+const PORT = 3000;
+
+mongoose.connect('mongodb+srv://tsrviswajeth:viswajeth@excel.4nctr3z.mongodb.net/Excel?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  user_id:Number,
+  name: String,
+  age: Number,
+  email: String,
+  phone: Number,
+  postal_code: Number,
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+// Middleware to parse JSON body
+app.use(express.json());
+async function gsrun(cli){
+  
+  app.get('/create_user',async(req,res)=>{
+  try{
+    const gsapi=google.sheets({version:'v4',auth:cli});
+  const opt={
+    spreadsheetId:'17itjdorhyRMvxnyQFij2iR6MFwyxMqIXzh-gBqSBFLo',
+    range:'Sheet1!A3:F3'
+  };
+   var data = await gsapi.spreadsheets.values.get(opt);
+    const dataArray=data.data.values;
+    const user = {
+      user_id: dataArray[0][0],
+      name: dataArray[0][1],
+      age: dataArray[0][2],
+      email: dataArray[0][3],
+      phone: dataArray[0][4],
+      postal_code: dataArray[0][5]
+    }
+    const createdUser = await User.insertMany(user);
+    res.status(201).json(createdUser);
+    
+  }catch(err){
+    res.status(500).json({err:"Internal Server Error"});
+  }
+
+  });
+
+  
+  app.get('/delete_user',async(req,res)=>{
+    try{
+      const gsapi=google.sheets({version:'v4',auth:cli});
+    const opt={
+      spreadsheetId:'17itjdorhyRMvxnyQFij2iR6MFwyxMqIXzh-gBqSBFLo',
+      range:'Sheet1!A7'
+    };
+      const data = await gsapi.spreadsheets.values.get(opt);
+      const userId=data.data.values;
+      await User.deleteOne({_id : userId });
+      res.status(200).json({message:"User deleted successfully"});
+     
+    }catch(err){
+      res.status(500).json({err:"Internal Server Error"});
+    }
+  
+    });
+
+    app.get('/update_user',async(req,res)=>{
+      try{
+        const gsapi=google.sheets({version:'v4',auth:cli});
+      const opt={
+        spreadsheetId:'17itjdorhyRMvxnyQFij2iR6MFwyxMqIXzh-gBqSBFLo',
+        range:'Sheet1!A13:G13'
+      };
+       var data = await gsapi.spreadsheets.values.get(opt);
+        const dataArray=data.data.values;
+        const user = {
+          user_id: dataArray[0][0],
+          name: dataArray[0][1],
+          age: dataArray[0][2],
+          email: dataArray[0][3],
+          phone: dataArray[0][4],
+          postal_code: dataArray[0][5]
+        }
+        const userId={user_id:dataArray[0][0]}
+        await User.updateOne(userId, user);
+        res.status(200).json({ message: 'User updated successfully' });
+        
+      }catch(err){
+        res.status(500).json({err:"Internal Server Error"});
+      }
+    
+      });
+      app.get('/read_users',async(req,res)=>{
+        try{
+            const read=require('./read.js')
+          
+          res.status(200).json({ message: 'Users populated in the Google Sheet' });
+        
+        }catch(err){
+          res.status(500).json({err:"Internal Server Error"});
+        }
+      
+        });
+
+
+
+}
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
---
+++ read.js
@@ -0,0 +1,62 @@
+const { google } = require('googleapis');
+const { MongoClient } = require('mongodb');
+
+const mongoUri = 'mongodb+srv://tsrviswajeth:viswajeth@excel.4nctr3z.mongodb.net/Excel?retryWrites=true&w=majority';
+const dbName='Excel';
+const collectionName = 'users';
+const spreadsheetId = '17itjdorhyRMvxnyQFij2iR6MFwyxMqIXzh-gBqSBFLo';
+const credentials = require('./credentials.json');
+
+async function readDataFromMongoDB() {
+  try {
+    const client = await MongoClient.connect(mongoUri);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const cursor = collection.find({},{ _id: 0 });
+    const data = await cursor.toArray();
+    const dataArray = data.map(obj => Object.values(obj));
+    client.close();
+    return dataArray;
+  } catch (err) {
+    console.error('Error:', err);
+    return [];
+  }
+}
+
+async function writeToGoogleSheet(dataArray) {
+  try {
+    // Authenticate with Google Sheets API using credentials
+    const auth = await google.auth.getClient({
+      credentials,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const sheets = google.sheets({ version: 'v4', auth });
+    const startRow = 19; 
+    const range = `Sheet1!A${startRow}`;
+    const request = {
+      spreadsheetId,
+      range,
+      valueInputOption: 'RAW',
+      resource: {
+        values: dataArray,
+      },
+    };
+
+    // Make the API request to insert data
+    await sheets.spreadsheets.values.update(request);
+
+    console.log('Data written to Google Sheet successfully.');
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
+
+async function main() {
+  const dataArray = await readDataFromMongoDB();
+  await writeToGoogleSheet(dataArray);
+}
+main();
+
+module.exports={
+    main
+}
